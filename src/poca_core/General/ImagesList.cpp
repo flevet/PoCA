227eb82824899943a046d2ff30025da9a91b3024
Florian Levet <florian.levet@u-bordeaux.fr>
2025-05-19 17:55:52 +0200

    Modifying image to imagesList

---
+++ src/poca_core/General/ImagesList.cpp
@@ -0,0 +1,111 @@
+/*
+* Software:  PoCA: Point Cloud Analyst
+*
+* File:      ImagesList.cpp
+*
+* Copyright: Florian Levet (2020-2025)
+*
+* License:   LGPL v3
+*
+* Homepage:  https://github.com/flevet/PoCA
+*
+* PoCA is a free software; you can redistribute it and/or
+* modify it under the terms of the GNU Lesser General Public
+* License as published by the Free Software Foundation; either
+* version 3 of the License, or (at your option) any later version.
+*
+* The algorithms that underlie PoCA have required considerable
+* development. They are described in the original SR-Tesseler paper,
+* doi:10.1038/nmeth.3579. If you use PoCA as part of work (visualization,
+* manipulation, quantification) towards a scientific publication, please include
+* a citation to the original paper.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+* Lesser General Public License for more details.
+*
+* You should have received a copy of the GNU Lesser General Public License
+* along with this program; if not, write to the Free Software Foundation,
+* Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
+#include <Interfaces/ImageInterface.hpp>
+#include <General/BasicComponentList.hpp>
+
+#include "ImagesList.hpp"
+
+namespace poca::core {
+	ImagesList::ImagesList(ImageInterface* _obj, const std::string& _name) :BasicComponentList("ImagesList")
+	{
+		m_components.push_back(_obj);
+		m_currentComponent = 0;
+		m_names.push_back(_name);
+	}
+
+	ImagesList::~ImagesList() {
+
+	}
+
+	BasicComponentInterface* ImagesList::copy()
+	{
+		return new ImagesList(*this);
+	}
+
+	void ImagesList::addImage(ImageInterface* _obj, const std::string& _name)
+	{
+		addComponent(_obj);
+		m_names.push_back(_name);
+	}
+
+	ImageInterface* ImagesList::currentImage()
+	{
+		return static_cast<ImageInterface*>(m_components[m_currentComponent]);
+	}
+
+	uint32_t ImagesList::currentImageIndex() const
+	{
+		return m_currentComponent;
+	}
+
+	ImageInterface* ImagesList::getImage(const uint32_t _idx)
+	{
+		return static_cast<ImageInterface*>(m_components[_idx]);
+	}
+
+	void ImagesList::eraseImage(const uint32_t _index)
+	{
+		if (m_components.empty()) return;
+		BasicComponentList::eraseComponent(_index);
+		m_names.erase(m_names.begin() + _index);
+	}
+
+	const std::string& ImagesList::currentName() const
+	{
+		if (m_currentComponent < m_components.size())
+			return m_names[m_currentComponent];
+		else
+			return std::string("");
+	}
+
+	const std::string& ImagesList::getName(const uint32_t _index) const
+	{
+		if (_index < m_components.size())
+			return m_names[_index];
+		else
+			return std::string("");
+	}
+
+	void ImagesList::setName(const uint32_t _index, const std::string& _name)
+	{
+		if (_index < m_components.size())
+			m_names[_index] = _name;
+	}
+
+	void ImagesList::setCurrentName(const std::string& _name)
+	{
+		if (m_currentComponent < m_components.size())
+			m_names[m_currentComponent] = _name;
+	}
+}
+
---
+++ src/poca_core/General/ImagesList.hpp
@@ -0,0 +1,68 @@
+/*
+* Software:  PoCA: Point Cloud Analyst
+*
+* File:      ImagesList.hpp
+*
+* Copyright: Florian Levet (2020-2025)
+*
+* License:   LGPL v3
+*
+* Homepage:  https://github.com/flevet/PoCA
+*
+* PoCA is a free software; you can redistribute it and/or
+* modify it under the terms of the GNU Lesser General Public
+* License as published by the Free Software Foundation; either
+* version 3 of the License, or (at your option) any later version.
+*
+* The algorithms that underlie PoCA have required considerable
+* development. They are described in the original SR-Tesseler paper,
+* doi:10.1038/nmeth.3579. If you use PoCA as part of work (visualization,
+* manipulation, quantification) towards a scientific publication, please include
+* a citation to the original paper.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+* Lesser General Public License for more details.
+*
+* You should have received a copy of the GNU Lesser General Public License
+* along with this program; if not, write to the Free Software Foundation,
+* Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
+#ifndef ImagesList_hpp__
+#define ImagesList_hpp__
+
+
+#include <General/BasicComponentList.hpp>
+
+namespace poca::core {
+	class ImageInterface;
+
+	class ImagesList : public BasicComponentList {
+	public:
+		ImagesList(ImageInterface*, const std::string & = "");
+		~ImagesList();
+
+		BasicComponentInterface* copy();
+
+		void addImage(ImageInterface*, const std::string & = "");
+		ImageInterface* currentImage();
+		ImageInterface* getImage(const uint32_t);
+		uint32_t currentImageIndex() const;
+
+		void eraseCurrentImage() { eraseImage(m_currentComponent); }
+		void eraseImage(const uint32_t);
+
+		const std::string& currentName() const;
+		const std::string& getName(const uint32_t _index) const;
+		void setName(const uint32_t _index, const std::string& _name);
+		void setCurrentName(const std::string& _name);
+
+
+	protected:
+		std::vector <std::string> m_names;
+	};
+}
+#endif
+
