f9abf5a2809d5ca2c2b053865f23655b9a10ba61
Florian Levet <florian.levet@u-bordeaux.fr>
2025-06-25 18:09:17 +0200

    Widget for customized slider

---
+++ src/poca_plot/Plot/CustomizedSlider.cpp
@@ -0,0 +1,124 @@
+/*
+* Software:  PoCA: Point Cloud Analyst
+*
+* File:      CustomizedSlider.cpp
+*
+* Copyright: Florian Levet (2020-2025)
+*
+* License:   LGPL v3
+*
+* Homepage:  https://github.com/flevet/PoCA
+*
+* PoCA is a free software; you can redistribute it and/or
+* modify it under the terms of the GNU Lesser General Public
+* License as published by the Free Software Foundation; either
+* version 3 of the License, or (at your option) any later version.
+*
+* The algorithms that underlie PoCA have required considerable
+* development. They are described in the original SR-Tesseler paper,
+* doi:10.1038/nmeth.3579. If you use PoCA as part of work (visualization,
+* manipulation, quantification) towards a scientific publication, please include
+* a citation to the original paper.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+* Lesser General Public License for more details.
+*
+* You should have received a copy of the GNU Lesser General Public License
+* along with this program; if not, write to the Free Software Foundation,
+* Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
+#include "CustomizedSlider.hpp"
+
+namespace poca::plot {
+	CustomizedSlider::CustomizedSlider(float _min, float _max, int nbSteps, QWidget* parent)
+		: QWidget(parent),
+		m_minVal(_min),
+		m_maxVal(_max),
+		m_sliderSteps(nbSteps)
+	{
+		// Slider
+		m_slider = new QSlider(Qt::Horizontal);
+		m_slider->setRange(0, m_sliderSteps);
+		m_slider->setValue(0);  // initial position
+
+		// Min / Max labels
+		m_minLEdit = new QLineEdit(QString::number(m_minVal, 'f', 3));
+		m_maxLEdit = new QLineEdit(QString::number(m_maxVal, 'f', 3));
+
+		// Layout: single row
+		QHBoxLayout* hLayout = new QHBoxLayout;
+		hLayout->addWidget(m_minLEdit);
+		hLayout->addWidget(m_slider);
+		hLayout->addWidget(m_maxLEdit);
+
+		setLayout(hLayout);
+
+		// Live tooltip during drag
+		connect(m_slider, &QSlider::sliderMoved, this, &CustomizedSlider::showTooltip);
+
+		// Emit signal when value changes (optional)
+		connect(m_slider, &QSlider::valueChanged, this, &CustomizedSlider::emitValueChanged);
+	
+	
+		connect(m_minLEdit, &QLineEdit::editingFinished, this, &CustomizedSlider::modifyMinMax);
+		connect(m_maxLEdit, &QLineEdit::editingFinished, this, &CustomizedSlider::modifyMinMax);
+	}
+
+	float CustomizedSlider::value() const
+	{
+		return m_minVal + (m_maxVal - m_minVal) * (float(m_slider->value()) / m_sliderSteps);
+	}
+
+	void CustomizedSlider::setMaxValue(float _val)
+	{
+		m_maxVal = _val;
+	}
+
+	void CustomizedSlider::showTooltip(int value)
+	{
+		float alpha = m_minVal + (m_maxVal - m_minVal) * (float(value) / m_sliderSteps);
+
+		// Get slider handle position using QStyle
+		QStyleOptionSlider opt;
+		opt.initFrom(m_slider);
+		opt.orientation = m_slider->orientation();
+		opt.minimum = m_slider->minimum();
+		opt.maximum = m_slider->maximum();
+		opt.sliderPosition = value;
+		opt.sliderValue = value;
+
+		QRect handleRect = m_slider->style()->subControlRect(
+			QStyle::CC_Slider, &opt, QStyle::SC_SliderHandle, m_slider);
+
+		QPoint handleCenter = m_slider->mapToGlobal(handleRect.center());
+		QPoint tooltipPos(handleCenter.x(), handleCenter.y() - 30);  // offset above handle
+
+		QToolTip::showText(tooltipPos, QString::number(alpha, 'f', 3), m_slider);
+	}
+
+	void CustomizedSlider::emitValueChanged(int value)
+	{
+		float alpha = m_minVal + (m_maxVal - m_minVal) * (float(value) / m_sliderSteps);
+		emit changedValue(alpha);
+	}
+
+	void CustomizedSlider::modifyMinMax()
+	{
+		QObject* sender = QObject::sender();
+		if (sender == m_minLEdit) {
+			bool ok;
+			float tmp = m_minLEdit->text().toFloat(&ok);
+			if (ok)
+				m_minVal = tmp;
+		}
+		else if (sender == m_maxLEdit) {
+			bool ok;
+			float tmp = m_maxLEdit->text().toFloat(&ok);
+			if (ok)
+				m_maxVal = tmp;
+		}
+	}
+}
---
+++ src/poca_plot/Plot/CustomizedSlider.hpp
@@ -0,0 +1,72 @@
+/*
+* Software:  PoCA: Point Cloud Analyst
+*
+* File:      CustomizedSlider.hpp
+*
+* Copyright: Florian Levet (2020-2025)
+*
+* License:   LGPL v3
+*
+* Homepage:  https://github.com/flevet/PoCA
+*
+* PoCA is a free software; you can redistribute it and/or
+* modify it under the terms of the GNU Lesser General Public
+* License as published by the Free Software Foundation; either
+* version 3 of the License, or (at your option) any later version.
+*
+* The algorithms that underlie PoCA have required considerable
+* development. They are described in the original SR-Tesseler paper,
+* doi:10.1038/nmeth.3579. If you use PoCA as part of work (visualization,
+* manipulation, quantification) towards a scientific publication, please include
+* a citation to the original paper.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+* Lesser General Public License for more details.
+*
+* You should have received a copy of the GNU Lesser General Public License
+* along with this program; if not, write to the Free Software Foundation,
+* Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
+#ifndef CustomizedSlider_h__
+#define CustomizedSlider_h__
+
+#include <QtWidgets/QSpinBox>
+#include <QtWidgets/QSlider>
+#include <QtWidgets/QLineEdit>
+#include <QtWidgets/QStyleOptionSlider>
+#include <QtWidgets/QHBoxLayout>
+#include <QtWidgets/QToolTip>
+
+namespace poca::plot {
+	class CustomizedSlider : public QWidget
+	{
+		Q_OBJECT
+
+	public:
+		explicit CustomizedSlider(float _min, float _max, int nbSteps, QWidget* parent = nullptr);
+
+		float value() const;
+		void setMaxValue(float);
+
+		void modifyMinMax();
+
+	signals:
+		void changedValue(float alpha);
+
+	private:
+		QSlider* m_slider;
+		QLineEdit* m_minLEdit, * m_maxLEdit;
+		float m_minVal;
+		float m_maxVal;
+		int m_sliderSteps;
+
+		void showTooltip(int value);
+
+		void emitValueChanged(int value);
+	};
+}
+
+#endif
